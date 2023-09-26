54357fdcffd064aa8e9b2686999d841c81a13787
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2023-09-26 15:31:57 -0300

    create out dir if not exists

--- utils/json.c
+++ utils/json.c
@@ -1,13 +1,22 @@
 #ifndef JSON_UTILS
 #define JSON_UTILS
 
+#define DIRNAME "out"
 #define FILENAME "out/data.json"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 void json_begin() {
+    struct stat st = {0};
+
+    if (stat(DIRNAME, &st) == -1) {
+        mkdir(DIRNAME, 0700);
+    }
+
     FILE *file = fopen(FILENAME, "w");
     fprintf(file, "{\n");
     fclose(file);
